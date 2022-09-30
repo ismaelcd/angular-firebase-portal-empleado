30c10fe768e9ba8d07cdaf4e9286eaf920d65537
icd1972 <ismael.cruz@unionsecurity.io>
2022-09-30 21:29:12 +0200

    Continuando con el proyecto

--- empleados/src/app/app-routing.module.ts
+++ empleados/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListEmpleadosComponent } from './componets/list-empleados/list-empleados.component';
+import { CreateEmpleadoComponent } from './componets/create-empleado/create-empleado.component';
 
-const routes: Routes = [];
+/* Aqui se definen las URL del proyecto */
+const routes: Routes = [
+  /* Especificamos la ruta por defecto del proyecto */
+  { path: '', redirectTo: 'list-empleados', pathMatch: 'full' },
+  { path: 'list-empleados', component: ListEmpleadosComponent },
+  { path: 'create-empleado', component: CreateEmpleadoComponent },
+  /* Si la URL no es valida, redirecciona a la URL por defecto. Siempre tiene que estar en la ultima posicion */
+  { path: '**', redirectTo: 'list-empleados', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
